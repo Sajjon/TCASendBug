a2e5df632b3cd6d4381e68a1253e47323c16af56
Alexander Cyon <alex.cyon@gmail.com>
2023-11-22 18:34:18 +0100

    create project file

---
+++ TCASendBug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCASendBug
+//
+//  Created by Alexander Cyon on 2023-11-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCASendBug/TCASendBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASendBugApp.swift
+//  TCASendBug
+//
+//  Created by Alexander Cyon on 2023-11-22.
+//
+
+import SwiftUI
+
+@main
+struct TCASendBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
