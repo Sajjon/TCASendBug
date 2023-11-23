772753356f45338a7b69ac80ef528bd3892a82b4
Alexander Cyon <alex.cyon@gmail.com>
2023-11-23 20:51:41 +0100

    WIP

--- TCASendBug/AppFeature.swift
+++ TCASendBug/AppFeature.swift
@@ -15,67 +15,61 @@ let log = Logger()
 @Reducer
 struct App {
 	
+	// ‼️‼️‼️‼️‼️‼️‼️‼️‼️
+	// Here is the bug! This reducer (`App`) never received the action `successfullyFinished`, even though
+	// we send it in `run` below, after `await slowTask` has finished.
+	//
+	// If we skip setting `state.modal = ni` (skip dismissing the modal (we REALLY want to dismiss it)),
+	// then no bug. Or the `slowTask` is not that slow, then it works. But this is a small demo, in real life
+	// the `slowTask` is a network request of variable speed, so we cannot control how slow it is.
+	//
+	// And our UX dictactes that we must dismiss the modal. So neither of the "fixes" is acceptable.
+	// ‼️‼️‼️‼️‼️‼️‼️‼️‼️
 	func proceedToNextSlowTask(state: inout State) -> Effect<Action> {
 		state.status = .requestInFlight
-		state.destination = nil // <-- COMMENTING OUT THIS (which we dont) "fixes" the bug. As in `run` below does not get cancelled
+		state.modal = nil // 💡 <-- COMMENTING OUT THIS ✅🐞 NO BUG
 		return .run { send in
-			await slowTask(name: "App")
+			await slowTask(name: "App", iterations: 1000) // 💡<--- OR IF YOU LOWER `iteration` to e.g `1000` ✅🐞 NO BUG
 			log.notice("`await send(.successfullyFinished)` is about to get called...")
 			await send(.successfullyFinished)
 			log.notice("`await send(.successfullyFinished)` called? Ever received???")
 		}
 	}
 	
-	
 	var body: some ReducerOf<App> {
 		Reduce { state, action in
 			switch action {
-			case .destination(.presented(.modal(.done))):
+			case .modal(.presented(.done)):
 				return proceedToNextSlowTask(state: &state)
 			case .successfullyFinished:
 				log.notice("Successfully recevied `successfullyFinished`! No bug!")
-				state.destination = nil
+				state.modal = nil
 				state.status = .successfullyFinished
 				return .none
 			default: return .none
 			}
 		}
-		.ifLet(\.$destination, action: /Action.destination) {
-			Destination()
+		.ifLet(\.$modal, action: /Action.modal) {
+			Modal()
 		}
 	}
 	
 	struct State: Sendable, Hashable {
 		@PresentationState
-		var destination: Destination.State?
+		var modal: Modal.State?
 		enum Status: String, Sendable, Hashable {
 			case idle, requestInFlight, successfullyFinished
 		}
 		var status: Status = .idle
 		init() {
-			log.debug("App start")
-			self.destination = .modal(.init())
+			self.modal = .init()
 		}
 	}
 	enum Action: Sendable, Equatable {
-		case destination(PresentationAction<Destination.Action>)
+		case modal(PresentationAction<Modal.Action>)
 		case successfullyFinished
 	}
-	struct Destination: Reducer {
-		enum State: Sendable, Hashable {
-			case modal(Modal.State)
-		}
 
-		enum Action: Sendable, Equatable {
-			case modal(Modal.Action)
-		}
-
-		var body: some ReducerOf<Self> {
-			Scope(state: /State.modal, action: /Action.modal) {
-				Modal()
-			}
-		}
-	}
 	struct View: SwiftUI.View {
 		let store: StoreOf<App>
 		var body: some SwiftUI.View {
@@ -96,9 +90,7 @@ struct App {
 					Spacer()
 				}
 				.sheet(
-					store: store.scope(state: \.$destination, action: { .destination($0) }),
-					state: /App.Destination.State.modal,
-					action: App.Destination.Action.modal,
+					store: store.scope(state: \.$modal, action: { .modal($0) }),
 					content: {
 						Modal.View(store: $0)
 					}
@@ -109,10 +101,10 @@ struct App {
 	
 }
 
-func slowTask(name: String) async {
+func slowTask(name: String, iterations: Int = 10000) async {
 	log.info("Slow task - '\(name)' START")
 	await Task {
-		_ = (0...20000).map { _ in
+		_ = (0...iterations).map { _ in
 			Curve25519.Signing.PrivateKey().publicKey
 		}
 	}.value
