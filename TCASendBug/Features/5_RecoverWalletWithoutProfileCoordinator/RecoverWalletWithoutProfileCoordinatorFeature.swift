addcc09a0d45436407a79e0ff188c3d14894060d
Alexander Cyon <alex.cyon@gmail.com>
2023-11-23 08:21:58 +0100

    Reach RecoverWalletWithoutProfileStart

--- TCASendBug/Features/5_RecoverWalletWithoutProfileCoordinator/RecoverWalletWithoutProfileCoordinatorFeature.swift
+++ TCASendBug/Features/5_RecoverWalletWithoutProfileCoordinator/RecoverWalletWithoutProfileCoordinatorFeature.swift
@@ -11,14 +11,54 @@ import SwiftUI
 
 public struct RecoverWalletWithoutProfileCoordinator: Sendable, FeatureReducer {
 	
+	@MainActor
 	public struct View: SwiftUI.View {
 		public let store: StoreOf<RecoverWalletWithoutProfileCoordinator>
+
+		public init(store: StoreOf<RecoverWalletWithoutProfileCoordinator>) {
+			self.store = store
+		}
+
 		public var body: some SwiftUI.View {
-			VStack {
-				Text("RecoverWalletWithoutProfileCoordinator")
-//				Button("Next") {
-//					store.send(.view(.next))
-//				}
+			NavigationStackStore(
+				store.scope(state: \.path, action: { .child(.path($0)) })
+			) {
+				IfLetStore(
+					store.scope(state: \.root, action: { .child(.root($0)) })
+				) {
+					path(for: $0)
+				}
+			} destination: {
+				path(for: $0)
+			}
+			.destinations(with: store)
+		}
+
+		private func path(
+			for store: StoreOf<RecoverWalletWithoutProfileCoordinator.Path>
+		) -> some SwiftUI.View {
+			SwitchStore(store) { state in
+				switch state {
+				case .start:
+					CaseLet(
+						/RecoverWalletWithoutProfileCoordinator.Path.State.start,
+						action: RecoverWalletWithoutProfileCoordinator.Path.Action.start,
+						then: { RecoverWalletWithoutProfileStart.View(store: $0) }
+					)
+//				case .importMnemonic:
+//					CaseLet(
+//						/RecoverWalletWithoutProfileCoordinator.Path.State.importMnemonic,
+//						action: RecoverWalletWithoutProfileCoordinator.Path.Action.importMnemonic,
+//						then: { ImportMnemonic.View(store: $0) }
+//					)
+//
+//				case .recoveryComplete:
+//					CaseLet(
+//						/RecoverWalletWithoutProfileCoordinator.Path.State.recoveryComplete,
+//						action: RecoverWalletWithoutProfileCoordinator.Path.Action.recoveryComplete,
+//						then: { RecoverWalletControlWithBDFSComplete.View(store: $0) }
+//					)
+				}
 			}
 		}
 	}
@@ -196,4 +236,32 @@ public struct RecoverWalletWithoutProfileCoordinator: Sendable, FeatureReducer {
 		return .send(.delegate(.dismiss))
 	}
  */
+}
+
+
+private extension StoreOf<RecoverWalletWithoutProfileCoordinator> {
+	var destination: PresentationStoreOf<RecoverWalletWithoutProfileCoordinator.Destination> {
+		func scopeState(state: State) -> PresentationState<RecoverWalletWithoutProfileCoordinator.Destination.State> {
+			state.$destination
+		}
+		return scope(state: scopeState, action: Action.destination)
+	}
+}
+
+private extension View {
+	@MainActor
+	func destinations(with store: StoreOf<RecoverWalletWithoutProfileCoordinator>) -> some View {
+		let destinationStore = store.destination
+		return accountRecoveryScanCoordinator(with: destinationStore)
+	}
+
+	@MainActor
+	private func accountRecoveryScanCoordinator(with destinationStore: PresentationStoreOf<RecoverWalletWithoutProfileCoordinator.Destination>) -> some View {
+		fullScreenCover(
+			store: destinationStore,
+			state: /RecoverWalletWithoutProfileCoordinator.Destination.State.accountRecoveryScanCoordinator,
+			action: RecoverWalletWithoutProfileCoordinator.Destination.Action.accountRecoveryScanCoordinator,
+			content: { AccountRecoveryScanCoordinator.View(store: $0) }
+		)
+	}
 }
